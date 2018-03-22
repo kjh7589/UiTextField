4bdd1e718d7fba4d64f41bca19f9b03c7776a0e7
D7703_25 <d7703_25@D7703-25ui-iMac.local>
2018-03-22 15:47:40 +0900

    clear

--- UiTextField/ViewController.swift
+++ UiTextField/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lab: UILabel!
+    @IBOutlet weak var labName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        labName.placeholder = "입력을 하세요"
+        labName.clearButtonMode = UITextFieldViewMode.whileEditing
+        labName.borderStyle = UITextBorderStyle.line
     }
 
+    @IBAction func bt(_ sender: Any) {
+        lab.text = "Hello " + labName.text!
+        labName.text = ""
+        
+        labName.resignFirstResponder()
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+         labName.resignFirstResponder()
+    }
 
 }
 
